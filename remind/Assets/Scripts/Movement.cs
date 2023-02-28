3773922a7f05b25885bc7dfeac5a77a2699f7251
adam mekkaoui <a.mekkaoui@student.saedubai,com>
2023-02-28 14:01:23 +0400

    added Movemnt code

---
+++ remind/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+
+    [SerializeField] public float speed = 1f;
+    private float movehorizontal;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        movehorizontal = Input.GetAxisRaw("Horizontal");
+
+    }
+    void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(movehorizontal * speed, 0f);
+    }
+
+}
